873058ad55c5c630314fb1abae018bd63b1ef4bb
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-02-06 14:10:20 +0100

    First commit

---
+++ src/main/java/crud/controller/PostController.java
@@ -0,0 +1,53 @@
+package crud.controller;
+
+import com.google.gson.Gson;
+import crud.model.Post;
+import crud.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private final Gson gson;
+
+
+    public PostController(PostService service) {
+        this.service = service;
+        gson = new Gson();
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        //установить Content Type (application/json) тип содержимого ответа, который мы собираемся отправить
+        prepareResponse(response);
+        //Collections.emptyList()
+        final var data = service.all();
+        //Конвертируем полученный List <Post> в json   getWriter() - Возвращает объект PrintWriter, который может отправлять символьный текст клиенту.
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        prepareResponse(response);
+        final var postById = service.getById(id);
+        response.getWriter().print(gson.toJson(postById));
+
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        prepareResponse(response);
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) {
+        prepareResponse(response);
+        service.removeById(id);
+    }
+
+    private void prepareResponse(HttpServletResponse response) {
+        response.setContentType(APPLICATION_JSON);
+    }
+}
---
+++ src/main/java/crud/servlet/MainServlet.java
@@ -0,0 +1,89 @@
+package crud.servlet;
+
+import crud.controller.PostController;
+import crud.repository.PostRepository;
+import crud.service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    public static final String API_POSTS = "/api/posts";
+    public static final String API_POSTS_D = "/api/posts/\\d+";
+    public static final String STR = "/";
+    private PostController controller;
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final var method = req.getMethod();
+            if (method.equals("GET")) {
+                doGet(req, resp);
+                return;
+            }
+            if (method.equals("POST")) {
+                doPost(req, resp);
+                return;
+            }
+            if (method.equals("DELETE")) {
+
+                doDelete(req, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+            resp.getWriter().printf("method '%s' or path '%s' not found", method, req.getRequestURI());
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final var path = req.getRequestURI();
+
+        if (path.equals(API_POSTS)) {
+            controller.all(resp);
+            return;
+        }
+        if (path.matches(API_POSTS_D)) {
+            final var id = Long.parseLong(path.substring(path.lastIndexOf(STR)));
+            controller.getById(id, resp);
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final var path = req.getRequestURI();
+        if (path.equals(API_POSTS)) {
+            controller.save(req.getReader(), resp);
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final var path = req.getRequestURI();
+        if (path.matches(API_POSTS_D)) {
+            final var id = Long.parseLong(path.substring(path.lastIndexOf(STR)));
+            controller.removeById(id, resp);
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+}
+
